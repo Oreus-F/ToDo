f800babca47dccc6ee6a182e6a681cb9db286598
Oreus-F <fourniol.lucas@gmail.com>
2025-07-03 15:33:19 +0200

    rename webpack.config.js into webpack.common.js start init

---
+++ webpack.common.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const HtmlPluginWebpack = require('html-webpack-plugin');
+
+
+module.exports = {
+
+    entry: './src/index.js',
+    output: {
+        filename: 'mainBundle.js',
+        
+    }
+}
