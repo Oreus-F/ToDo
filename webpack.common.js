8504d3e5a60f70c915e4bf13be58dda3ae9682aa
Oreus-F <fourniol.lucas@gmail.com>
2025-07-03 15:46:26 +0200

    init plugin / modules in webpack.common.js

--- webpack.common.js
+++ webpack.common.js
@@ -10,4 +10,28 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         clean: true,
     },
-}
+
+
+    plugin : [
+        new HtmlPluginWebpack({
+            template: './src/template.html',
+        }),
+    ],
+
+    module: {
+        rule: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif|svg)$/i,
+                type: 'asset/ressource',
+            },
+        ],
+    },
+};
