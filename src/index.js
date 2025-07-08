3e9fd530ab81cf75cb0c7f652988055f197668a7
Oreus-F <fourniol.lucas@gmail.com>
2025-07-08 18:11:33 +0200

    console.log test

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import { ProjectManager } from "./projectManager";
 
+import { formatDistance, format } from "date-fns";
 
 const control = ProjectManager();
 
-control.createTask('ranger les cerises', 'low', '05/07/25', 'par intensité de la couleur')
-control.createTask('rincer le cocotier', 'medium', '04/07/25', 'arroser à la base du cocotier avec le sceau dans le garage');
-control.createTask('visiter les grands-parents', 'high', '21/07', 'rapporter des fleurs pour leur anniversaire de mariage');
+control.createTask('ranger les cerises', 'low', '05/07/2025', 'par intensité de la couleur')
+control.createTask('rincer le cocotier', 'medium', '04/07/2025', 'arroser à la base du cocotier avec le sceau dans le garage');
+control.createTask('visiter les grands-parents', 'high', '21/07/2025', 'rapporter des fleurs pour leur anniversaire de mariage');
 
 
 console.table(control.getProjectTasksList());
@@ -13,9 +14,9 @@ console.table(control.getProjectTasksList());
 control.newProject('ToDoList');
 control.changeProject(1);
 
-control.createTask('dessiner le projet', 'high', '04/07/25', 'better plans better futur')
-control.createTask('évaluer les dessins', 'medium', '05/07/25', 'Je ne suis pas un expert ta vu');
-control.createTask('transmettre à la team', 'high', '05/07/25', 'Eux ils savent faire, pas moi');
+control.createTask('dessiner le projet', 'high', '04/07/2025', 'better plans better futur')
+control.createTask('évaluer les dessins', 'medium', '05/07/2025', 'Je ne suis pas un expert ta vu');
+control.createTask('transmettre à la team', 'high', '05/07/2025', 'Eux ils savent faire, pas moi');
 
 
 console.table(control.getProjectTasksList());
@@ -30,6 +31,9 @@ control.completeSelectedTask(2);
 console.table(control.getProjectCompleteTasks())
 
 
+console.log(control.getProjectSpecificTask(0).getTimeLeft())
+
+
 
 // FOR PARSING INFORMATIONS
 // const newInstance = function(value){
@@ -37,4 +41,3 @@ console.table(control.getProjectCompleteTasks())
 //     instance.ParsingTasks();
 //     return instance
 // }
-
