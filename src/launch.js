ec7e6553e9a6f66b8e3127c1e30b60e7f5d9e29f
Oreus-F <fourniol.lucas@gmail.com>
2025-07-19 21:06:32 +0200

    call launch storage

--- src/launch.js
+++ src/launch.js
@@ -1,12 +1,15 @@
 import { activateSidebarButtons } from "./activateButtonSidebar";
 import { ProjectManager } from "./projectManager";
 import { updateSidebarProjectList } from "./updateDOMProjectSidebar";
+import { launchStorage } from "./localStorage";
 
 
 
 const control = ProjectManager();
 
 const launchPage = function(){
+    launchStorage();
+
 
     updateSidebarProjectList(control.getProjectList())
     activateSidebarButtons();
