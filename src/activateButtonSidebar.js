9d3e592fa72df5ae970e01bccd9c7388905970e6
Oreus-F <fourniol.lucas@gmail.com>
2025-07-19 16:45:16 +0200

    call the logic to display sidebar menu

--- src/activateButtonSidebar.js
+++ src/activateButtonSidebar.js
@@ -1,4 +1,5 @@
 import { createDialogProjectName, activateFormNewProject } from "./sidebarDialogCreationDOM";
+import { displaySidebar } from "./sidebarButtonHelperDOM";
 
 const setUpAddProjectButton = function(){
     const addProjectButton = document.querySelector('#addProjectButton');
@@ -10,8 +11,21 @@ const setUpAddProjectButton = function(){
 };
 
 
+const setUpDisplaySidebar = function(){
+    const displaySidebarButton = document.querySelector('#displaySidebarButton');
+
+    displaySidebarButton.addEventListener('click', (event) => {
+        let target = event.target;
+        target = target.parentElement;
+
+        displaySidebar(target)
+    });
+}
+
+
 const activateSidebarButtons = function(){
     setUpAddProjectButton()
+    setUpDisplaySidebar();
 }
 
 export {activateSidebarButtons}
