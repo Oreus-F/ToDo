ebbc8890694e055660cc42e2bdc7f58d548d410f
Oreus-F <fourniol.lucas@gmail.com>
2025-07-23 13:52:00 +0200

    reset dialog design

--- src/index.js
+++ src/index.js
@@ -6,43 +6,42 @@ import { ProjectManager } from './projectManager';
 
 const control = ProjectManager();
 
-// FOR PARSING INFORMATIONS
 
 launchPage();
 
 
-const editPreview = document.querySelector('#userPreviewPicture');
-// const currentPreview = localStorage.getItem('profilePicture');
-// editPreview.setAttribute('src', currentPreview);
+// const editPreview = document.querySelector('#userPreviewPicture');
+// // const currentPreview = localStorage.getItem('profilePicture');
+// // editPreview.setAttribute('src', currentPreview);
 
 
-const fileUploader = document.querySelector('#fileUploader');
+// const fileUploader = document.querySelector('#fileUploader');
 
-fileUploader.addEventListener('change', () => {
-    const file = fileUploader.files[0];
+// fileUploader.addEventListener('change', () => {
+//     const file = fileUploader.files[0];
 
-    const reader = new FileReader();
+//     const reader = new FileReader();
 
 
-    if (file){
-        readFileReader(file, reader)
-    }
+//     if (file){
+//         readFileReader(file, reader)
+//     }
     
-})
+// })
 
 
-const readFileReader = function(file, fileReader){
-    fileReader.addEventListener('load', () => {
-        editPreview.setAttribute('src', fileReader.result);
+// const readFileReader = function(file, fileReader){
+//     fileReader.addEventListener('load', () => {
+//         editPreview.setAttribute('src', fileReader.result);
 
 
-        // return a value and do this when submit the form instead of save it with the preview only
-        localStorage.setItem('profilePicture', fileReader.result)
-    });
+//         // return a value and do this when submit the form instead of save it with the preview only
+//         localStorage.setItem('profilePicture', fileReader.result)
+//     });
     
-    fileReader.readAsDataURL(file)
+//     fileReader.readAsDataURL(file)
     
-}
+// }
 
 
 const editUserDialog = document.querySelector('#editUserDialog');
