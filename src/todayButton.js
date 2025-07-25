fc8c82158e3415ce9b84904fa57e0b130839c02d
Oreus-F <fourniol.lucas@gmail.com>
2025-07-25 17:03:08 +0200

    style and organised headerDiv in today.js

--- src/todayButton.js
+++ src/todayButton.js
@@ -7,20 +7,42 @@ const createToday = function(){
 
 
     const headerDiv = document.createElement('div');
-    headerDiv.setAttribute('class', 'flex-basis120px flex-display');
+    headerDiv.setAttribute('class', 'flex-basis120px flex-display today-titleDiv-limits');
 
     const titleDiv = document.createElement('div');
-    titleDiv.setAttribute('class', 'flex-first-grow');
+    titleDiv.setAttribute('class', 'flex-first-grow flex-display column-direction justif-content-center ');
 
     const titleSpan = document.createElement('span');
+    titleSpan.setAttribute('class', 'flex-first-grow')
+
     const h1Title = document.createElement('h1');
+    h1Title.setAttribute('class', 'fs4r')
+    h1Title.textContent = 'Today';
 
-    
 
-    
+    titleSpan.appendChild(h1Title);
+    titleDiv.appendChild(titleSpan);
+
+
+    const divReminder = document.createElement('div');
+    divReminder.setAttribute('class', 'flex-basis40px flex-display aligned-item-end justif-content-end');
+
+    const spanIconReminder = document.createElement('span');
+    spanIconReminder.setAttribute('class', 'flex-basis40px full-h icon-today-tasks');
+
+    const spanTextReminder = document.createElement('span');
+    spanTextReminder.setAttribute('class', 'flex-first-grow');
+    spanTextReminder.textContent = 'message à modifier via JS'
+
+
+    divReminder.appendChild(spanIconReminder);
+    divReminder.appendChild(spanTextReminder);
+
+    titleDiv.appendChild(divReminder)
+
 
     const dateDiv = document.createElement('div');
-    dateDiv.setAttribute('class', 'flex-basis200px flex-display justif-content-center aligned-item-center');
+    dateDiv.setAttribute('class', 'flex-basis200px flex-display justif-content-end aligned-item-end');
 
     const dateSpan = document.createElement('span');
 
