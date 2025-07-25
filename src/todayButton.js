ac5aea7ba41b9bd29f23bee0f782a0af4659a9a4
Oreus-F <fourniol.lucas@gmail.com>
2025-07-25 16:14:51 +0200

    Create div's for title and date of today's window

--- src/todayButton.js
+++ src/todayButton.js
@@ -1,20 +1,45 @@
 
 
 
-const createTemplate_Today = function(){
+const createToday = function(){
     const div = document.createElement('div');
-    div.setAttribute('class', 'full-h');
+    div.setAttribute('class', 'full-h flex-display column-direction');
+
+
+    const headerDiv = document.createElement('div');
+    headerDiv.setAttribute('class', 'flex-basis120px flex-display');
+
+    const titleDiv = document.createElement('div');
+    titleDiv.setAttribute('class', 'flex-first-grow')
 
 
 
+    
+
+    const dateDiv = document.createElement('div');
+    dateDiv.setAttribute('class', 'flex-basis200px flex-display justif-content-center aligned-item-center');
+
+    const dateSpan = document.createElement('span');
+
+    const h3Date = document.createElement('h3');
+    h3Date.setAttribute('id', 'todayActualDate');
+    h3Date.textContent = '05/09/1995'
 
 
+    dateSpan.appendChild(h3Date);
+    dateDiv.appendChild(dateSpan);
+
+    headerDiv.appendChild(titleDiv);
+    headerDiv.appendChild(dateDiv);
 
 
+    const contentDiv = document.createElement('div');
+    contentDiv.setAttribute('class', 'flex-first-grow')
 
+    
 
-
-
+    div.appendChild(headerDiv);
+    div.appendChild(contentDiv);
 
 
     return div
@@ -26,7 +51,7 @@ const createTemplate_Today = function(){
 const displayTodayContent = function(){
     const content = document.querySelector('#content');
 
-    content.appendChild(createTemplate_Today())
+    content.appendChild(createToday())
 }
 
 
