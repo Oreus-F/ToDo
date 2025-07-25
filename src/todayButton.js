d518961e905425470ae836375cad71c9d2e09194
Oreus-F <fourniol.lucas@gmail.com>
2025-07-25 17:49:01 +0200

    Put a width to the scrollbar in order to make it invisible

--- src/todayButton.js
+++ src/todayButton.js
@@ -3,7 +3,7 @@ import { createContentTemplate } from "./contentTemplate";
 const displayTodayContent = function(){
     const content = document.querySelector('#content');
 
-    content.appendChild(createContentTemplate('Today', '05/09/2025', 'x tasks for today'))
+    content.appendChild(createContentTemplate('Today', '05/09/2025', 'x tasks for today'));
 }
 
 
