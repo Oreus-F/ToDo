7da55d474eb9e3c6f768c58ce7aac136bcd80a2f
Oreus-F <fourniol.lucas@gmail.com>
2025-07-25 15:21:54 +0200

    create a function to display and another to create template

--- src/todayButton.js
+++ src/todayButton.js
@@ -2,9 +2,32 @@
 
 
 const createTemplate_Today = function(){
+    const div = document.createElement('div');
+    div.setAttribute('class', 'full-h');
 
+
+
+
+
+
+
+
+
+
+
+
+
+    return div
 }
 
 
 
-export {createTemplate_Today}
+
+const displayTodayContent = function(){
+    const content = document.querySelector('#content');
+
+    content.appendChild(createTemplate_Today())
+}
+
+
+export {displayTodayContent}
