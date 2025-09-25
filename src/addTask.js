b79e02b65c9485e3aa159484377356c687ea8c86
Oreus-F <fourniol.lucas@gmail.com>
2025-09-25 15:46:37 +0200

    import the right function to use in aaddTask page

---
+++ src/addTask.js
@@ -0,0 +1,6 @@
+import { addDays, compareAsc, format, isBefore, isToday, isTomorrow } from "date-fns";
+import { createCalendarArray, getCalendarDays, getOneMonthAfter, getOneMonthBefore, getThisMonth } from "./bookingCalendar";
+import { ProjectManager } from "./projectManager";
+import { displayToday } from "./todayButton";
+
+
