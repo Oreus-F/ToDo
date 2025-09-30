c1fcac8988fc1d5435eef972eba84ee5281e5870
Oreus-F <fourniol.lucas@gmail.com>
2025-09-30 17:06:15 +0200

    create newBranch and new node to manipulate and try something

--- src/index.js
+++ src/index.js
@@ -10,3 +10,21 @@ import { getCalendarDays, createCalendarArray, getOneMonthBefore, getOneMonthAft
 launchPage();
 
 displayToday();
+
+
+const body = document.querySelector('body');
+
+const div1 = document.createElement('div');
+
+const div2 = document.createElement('div');
+
+const div3 = document.createElement('div');
+
+const div4 = document.createElement('div');
+
+const button = document.createElement('button');
+
+const panel = document.createElement('panel');
+
+
+body.appendChild('')
