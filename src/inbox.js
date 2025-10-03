767d81d398c5ad71ca545b4ac808194554d40aaf
Oreus-F <fourniol.lucas@gmail.com>
2025-10-03 17:29:09 +0200

    create displayInbox function with template

--- src/inbox.js
+++ src/inbox.js
@@ -2,3 +2,31 @@ import { createContentTemplate, taskTemplate } from "./contentTemplate";
 import { ProjectManager } from "./projectManager";
 import { format, isToday } from "date-fns";
 import { createInlineAddTask } from "./addTask";
+
+
+
+const displayInbox = function(){
+    const content = document.querySelector('#content');
+    
+    const TODAY = new Date();
+    
+    const projectList = control.getProjectList();
+    const inboxTasks = projectList[0].getTasksList();
+    
+    const TITLE_SECTION = "Inbox";
+    const TODAY_DATE = format(TODAY, 'dd/MM/yyyy');
+    const HEADER_DESCRIPTION = `${inboxTasks.length} tasks in Inbox !`
+    
+    content.appendChild(createContentTemplate(TITLE_SECTION, TODAY_DATE, HEADER_DESCRIPTION));
+    content.setAttribute('data-displayed', 'inbox');
+
+    const taskContainer = document.querySelector('#taskContainer');
+
+    taskContainer.appendChild(createInlineAddTask());
+    
+    // displayTasksToday(todayList)
+}
+
+
+
+export {displayInbox}
